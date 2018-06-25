78eef7d524b22f41f8659ae85d31ec67d5162e3e
Brian Hurst <brian.hurst@gsa.gov>
2018-06-24 22:10:25 -0400

    Add Jekyll

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,15 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './_src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'assets/js'),
+    filename: 'bundle.js',
   },
   devtool: 'cheap-module-source-map',
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
@@ -34,40 +21,19 @@ module.exports = {
               options: {
                 ident: 'postcss',
                 plugins: () => [
-                  require('tailwindcss')('./tailwind.js'),
+                  require('tailwindcss')('./_src/tailwind.js'),
                   require('autoprefixer')(),
                 ],
               },
             },
           ],
         }),
-      },
-      {
-        test: /\.(html)$/,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.(jpg|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]?[hash]',
-              outputPath: './images/',
-            },
-          },
-        ],
-      },
+      }
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
     new ExtractTextPlugin({
-      filename: 'style.[hash].css',
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new CompressionPlugin(),
+      filename: '../css/style.css',
+    })
   ],
 };
